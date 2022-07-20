00a690eaf09053943a26db743c9cd8d83d10f634
K0zak-A1exandr <101020248+K0zak-A1exandr@users.noreply.github.com>
2022-07-20 12:15:17 +0300

    Update quadratic.c

--- quadratic.c
+++ quadratic.c
@@ -1,20 +1,22 @@
+// GitHub
 
-#define EPSILON 0.00001
+
+#define EPSILON 0.00001 // TODO
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <assert.h>
-enum NRoots_e
+#include "quadratic.h"
+
+enum NRoots_e 
 {
-    INF_ROOTS     = 4,
+    NO_ROOTS      = 0,
     ONE_ROOTS     = 1,
     TWO_ROOTS     = 2,
-    NO_ROOTS      = 0,
     SAME_ROOTS    = 3,
+    INF_ROOTS     = 4,
 };
-
-
 
 
 int is_zero(double u)
@@ -23,35 +25,39 @@ int is_zero(double u)
 }
 
 int solver(double a, double b, double c, double *x1, double *x2)
-{   
-int result = scanf ("%lg %lg %lg", &a, &b, &c);
-    if (result < 3)
+{
+    int result = scanf ("%lg %lg %lg", &a, &b, &c); // Gets values of cooficients from keyboard 
+    
+    if (result < 3) // repeat scan
     {
         printf("Did your cat walk on the keyboard?\n");
         return 0;
     }
-        assert(a != NAN || b != NAN || c != NAN);
-    int numRoots = solve_square(a, b, c, &x1, &x2);
-            switch(numRoots)
-            {
-
+    assert(a != NAN || b != NAN || c != NAN);       // проверка на ввод числа, иначе выдать ошибку (или попросить заново ввести)
+    int numRoots = solve_square(a, b, c, x1, x2);
+        switch(numRoots)
+        {
             case NO_ROOTS:
                 printf(" No Solution\n");
                 break;
             case ONE_ROOTS:
-                printf(" x = %lg\n", x1 );
+                printf(" x = %lg\n", *x1 );
                 break;
             case TWO_ROOTS:
-                printf(" x1 = %lg\n x2 = %lg\n", x1, x2);
+                printf(" x1 = %lg\n x2 = %lg\n", *x1, *x2);
+                break;
+            case SAME_ROOTS:
+                printf(" x1 = x2 = %lg\n", *x1);
                 break;
             case INF_ROOTS:
                 printf(" Any number\n");
                 break;
-            case SAME_ROOTS:
-                printf(" x1 = x2 = %lg\n", x1);
-                break;
-            }
+            default:
+                printf("Big brain move\n");
+            return 0;
+        }
 }
+
 int solve_square(double a, double b, double c, double *x1, double *x2)
 {
     if(is_zero(a))
@@ -68,8 +74,9 @@ int solve_square(double a, double b, double c, double *x1, double *x2)
             }
         }
         else
-        {   printf("shit");
-            *x1 = - c / b;
+        {  
+            printf("%lg %lg\n", b, c);
+            *x1 = - c / b; 
             return ONE_ROOTS;
         }
     }
@@ -104,3 +111,4 @@ int solve_square(double a, double b, double c, double *x1, double *x2)
     }
     return 0;
 }
+
