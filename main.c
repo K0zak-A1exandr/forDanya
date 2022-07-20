51cf1930c1970d12c1a8897cccbb014845a5d3c6
K0zak-A1exandr <101020248+K0zak-A1exandr@users.noreply.github.com>
2022-07-20 12:13:19 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,27 @@
+// unit_tests в отдельный .h файл и .c файл
+// 
+// 
 
-#define EPSILON 0.00001
+#define EPSILON 0.00001 // TODO
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
-#include <assert.h>
-
+#include "quadratic.h"
 
 int main()
 {   
-   printf("             \n"
+   printf("              \n"
            "a*x^2+b*x+c  \n"
            "enter a b c: \n");
-    solver();
+
+    double a = 0;
+    double b = 0;
+    double c = 0;
+    double x1 = 0;
+    double x2 = 0;
+    int numsRot = solver(a, b, c, &x1, &x2);
+    
+    int unit_test();
+
+    return 0;
 }
