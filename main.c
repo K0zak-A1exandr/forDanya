314d5d7e1136f5cda2e777dbf4267a82bed05de1
K0zak-A1exandr <101020248+K0zak-A1exandr@users.noreply.github.com>
2022-08-01 11:58:41 +0300

    Update main.c

--- main.c
+++ main.c
@@ -5,22 +5,33 @@
 #define EPSILON 0.00001 // TODO
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include "quadratic.h"
+#include "unit_test.h"
 
 int main()
 {   
-   printf("              \n"
-           "a*x^2+b*x+c  \n"
-           "enter a b c: \n");
+    printf("              \n"
+           "a*x^2+b*x+c   \n"
+           "enter a b c:  \n");
 
     double a = 0;
     double b = 0;
     double c = 0;
     double x1 = 0;
     double x2 = 0;
-    int giga = unit_test(a, b, c, &x1, &x2);
-    int numsRot = solver(a, b, c, &x1, &x2);
+   // int giga = unit_test(a, b, c, &x1, &x2);
+   // int numsRot = solver(a, b, c, &x1, &x2);
+    solver(a, b, c, &x1, &x2);
 
-    return 0;
+    /*char* s = "1\0\04\0\02\0\0-0.585786\0\0-3.41421\0\02";
+    char* s1 = s;
+    char* s2 = s + 3 * sizeof(char);
+
+    char* strS = "1.12";
+    double strD = atof(strS);
+    printf("str - %s, double - %g\n", strS, strD);
+   
+    return 0;*/
 }
