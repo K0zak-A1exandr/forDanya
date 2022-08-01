482cb85a7dfc4e6a7ed10817ed4157d695154f2c
K0zak-A1exandr <101020248+K0zak-A1exandr@users.noreply.github.com>
2022-08-01 11:59:58 +0300

    Update unit_test.h

--- unit_test.h
+++ unit_test.h
@@ -1,12 +1,3 @@
-#ifndef UNIT_TEST_H_
-#define UNIT_TEST_H_
-
-#ifndef UNIT_TEST_H_
-#define UNIT_TEST_H_
-
 int unit_test();
 int is_zero(double u);
 
-#endif // UNIT_TEST_H_
-
-#endif // UNIT_TEST_H_
