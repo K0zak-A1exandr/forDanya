c05842bab0f9298b97c6674cfdc4c3466603f69f
K0zak-A1exandr <101020248+K0zak-A1exandr@users.noreply.github.com>
2022-08-08 16:19:50 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,8 +2,6 @@
 // 
 // 
 
-#define EPSILON 0.00001 // TODO
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -21,17 +19,8 @@ int main()
     double c = 0;
     double x1 = 0;
     double x2 = 0;
-   // int giga = unit_test(a, b, c, &x1, &x2);
-   // int numsRot = solver(a, b, c, &x1, &x2);
+    //int giga = unit_test(a, b, c, &x1, &x2);
+    //int numsRot = solver(a, b, c, &x1, &x2);
     solver(a, b, c, &x1, &x2);
 
-    /*char* s = "1\0\04\0\02\0\0-0.585786\0\0-3.41421\0\02";
-    char* s1 = s;
-    char* s2 = s + 3 * sizeof(char);
-
-    char* strS = "1.12";
-    double strD = atof(strS);
-    printf("str - %s, double - %g\n", strS, strD);
-   
-    return 0;*/
 }
