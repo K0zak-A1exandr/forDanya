8960a2ed68177987fdf2cb73774d823a10bf424e
K0zak-A1exandr <101020248+K0zak-A1exandr@users.noreply.github.com>
2022-08-08 16:49:20 +0300

    Update main.c

--- main.c
+++ main.c
@@ -14,13 +14,16 @@ int main()
            "a*x^2+b*x+c   \n"
            "enter a b c:  \n");
 
-    double a = 0;
-    double b = 0;
-    double c = 0;
-    double x1 = 0;
-    double x2 = 0;
+
     //int giga = unit_test(a, b, c, &x1, &x2);
     //int numsRot = solver(a, b, c, &x1, &x2);
+    double a = NAN;
+    double b = NAN; 
+    double c = NAN; 
+    double x1 = NAN;
+    double x2 = NAN;
+
+
     solver(a, b, c, &x1, &x2);
 
 }
