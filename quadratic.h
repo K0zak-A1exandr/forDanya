b7246f6e09672f0859b8d54d3d31d1c25846beb2
K0zak-A1exandr <101020248+K0zak-A1exandr@users.noreply.github.com>
2022-08-20 12:20:14 +0300

    Update quadratic.h

--- quadratic.h
+++ quadratic.h
@@ -1,5 +1,12 @@
-#define EPSILON 0.00001 // TODO
+// GitHub
+/**
+ @file
+ ыыыыы
+ */
 
+/*!
+     \brief Говорящие константы, отвечающие за количество решений       
+ */
 enum NRoots_e 
 {
     NO_ROOTS      = 0,
@@ -8,15 +15,48 @@ enum NRoots_e
     INF_ROOTS     = 3,
 };
 
+/*!
+     \brief Функция-отец, связывающая другие в одно целое
+     Заносит значения a b c в функцию, решающую квадратное уравнение
+     Содержит                 
+ */
 int solver(double a, double b, double c, double *x1, double *x2);
 
-void solve_quadratic(); // TODO
 
-void solve_discr(); // TODO
+/*!
+     \brief функция, решающая дискриминант   
 
+
+ */
+void solve_discr(); 
+
+/*!
+     \brief функция, решающая квадратное уравнение          
+ */
 int solve_square(double a, double b, double c, double *x1, double *x2);
 
+/*!
+     \brief Сравнивает значение с нулём                 
+ */
 int is_zero(double u);
 
-int print_roots (numRoots, x1, x2);
+/*!
+     \brief функция, которая выводит решения                 
+ */
+int print_roots (int numRoots, double x1, double x2);
+
+/*!
+     \brief Подфункция, решающая линейное уравнение                
+ */
+int solve_linear(double b, double c, double *x1);
+
+/*!
+     \brief функция, котрая считывает коефициенты                
+ */
+double coef_read();
+
+/*!
+    \brief очищает буфер ввода
+*/
+void flush_input_buffer();
 
