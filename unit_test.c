21ec37ae3f904b024a928ef1616051109c906b8f
K0zak-A1exandr <101020248+K0zak-A1exandr@users.noreply.github.com>
2022-08-20 12:30:37 +0300

    Update unit_test.c

--- unit_test.c
+++ unit_test.c
@@ -6,7 +6,6 @@
 #include "unit_test.h"
 #include <stdlib.h>
 
-//TODO clang format
 
 
 int unit_test()
@@ -28,21 +27,41 @@ int unit_test()
     fscanf(input_file, "%d", &n_tests);
     printf("got %d tests\n", n_tests);
     
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 4; i++)
     {   
-        fscanf(input_file, "%lf %lf %lf %lf %lf %d", &a, &b, &c, &x_1_expected, &x_2_expected, &n_ans_expected);
+       
+
+        fscanf(input_file, "%lf %lf %lf", &a, &b, &c);
+        fscanf(input_file, "%lf %lf %d", &x_1_expected, &x_2_expected, &n_ans_expected);
+
         
-        int n_ans = solver (a, b, c, &x_1, &x_2 );
+        int n_ans = solve_square (a, b, c, &x_1, &x_2 );
 
-        printf("got %d roots , x1 = %lg , x2 = %lg \n\t", n_ans, x_1, x_2);
-        if (n_ans_expected == n_ans &&
-            is_zero (x_1_expected - x_1) &&
-            is_zero (x_2_expected - x_2))
-
-            printf ("Test number %3d - OK\n", i+1);
+        //printf("got %d roots , x1 = %lg , x2 = %lg \n\t", n_ans, x_1, x_2);
+        if (n_ans_expected == n_ans)
+        {
+        
+            if (is_zero (x_1_expected - x_1) &&
+            is_zero (x_2_expected - x_2) ||
+            n_ans == 3 || n_ans == 1)
+            { 
+                printf ("Test number %d - OK\n", i+1);
+                printf ("Expected out: x1=%lg x2=%lg n_roots=%d\n", x_1_expected, x_2_expected, n_ans_expected);
+                printf ("Real     out: x1=%lg x2=%lg n_roots=%d\n\n", x_1, x_2, n_ans);
+            }
+            else   
+            {
+                printf ("ERROR\n");
+                printf ("Test number %d - ERROR\n", i+1);
+                printf ("In: a=%lg b=%lg c=%lg\n", a, b,c);
+                printf ("Expected out: x1=%lg x2=%lg n_roots=%d\n", x_1_expected, x_2_expected, n_ans_expected);
+                printf ("Real     out: x1=%lg x2=%lg n_roots=%d\n\n", x_1, x_2, n_ans);
+            }
+           
+        }
         else
         {
-            printf ("Test number %3d - ERROR\n", i+1);
+            printf ("Test number %d - ERROR\n", i+1);
             printf ("In: a=%lg b=%lg c=%lg\n", a, b,c);
             printf ("Expected out: x1=%lg x2=%lg n_roots=%d\n", x_1_expected, x_2_expected, n_ans_expected);
             printf ("Real     out: x1=%lg x2=%lg n_roots=%d\n\n", x_1, x_2, n_ans);
